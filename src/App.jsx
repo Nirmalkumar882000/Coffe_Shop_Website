023f8849511b0cb8be4574c5591cef4b97bf5d88
Lucky_World <128409984+Nirmalkumar882000@users.noreply.github.com>
2024-01-30 14:52:20 +0530

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
 
 
   return (
-    <div className='overflow-x-hidden'>
+    <div className=''>
      <Navbar/>
      <Home/>
      <Services/>
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -32,24 +32,26 @@ const Services = () => {
     <div className='py-10'>
      <div className="container">
       {/* Header  title  */}
-      <div className='text-center mb-20'>
-        <h1 className='text-4xl font-bold font-cursive text-gray-800'>Best Coffee for you</h1>
+      <div className='mb-20 text-center'>
+        <h1 className='text-4xl font-bold text-gray-800 font-cursive'>Best Coffee for you</h1>
       </div>
 
       {/* Service Card Section */}
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-14 place-items-center md:gap-5'>
-        <div>
           {
-            services.map((service,index)=>{
+            services.map((service)=>{
               return(
-              <div key={index} className='rounded-full'>
-                <div >
-                <img src={service.img} alt="coffee image"/>
+              <div
+              data-aos="fade-up"
+              data-aos-delay={service.aosDelay}
+                className='rounded-2xl bg-white hover:bg-primary hover:text-white relative shadow-xl duration-high group max-w-[300px]'>
+                <div className=''>
+                <img src={service.img} alt="coffee image" className='max-w-[200px] block'/>
                 </div>
               </div>
               )})
           }
-        </div>
+        
       </div>
 
 
